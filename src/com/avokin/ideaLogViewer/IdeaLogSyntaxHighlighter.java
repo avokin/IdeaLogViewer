1169bc8968edb33c9a784b5411760e204c142018
Andrey Vokin <andrey.vokin@gmail.com>
2017-11-06 09:27:41 +0100

    Make Error and Warn colors correspond to colors from console

--- src/com/avokin/ideaLogViewer/IdeaLogSyntaxHighlighter.java
+++ src/com/avokin/ideaLogViewer/IdeaLogSyntaxHighlighter.java
@@ -1,9 +1,9 @@
 package com.avokin.ideaLogViewer;
 
 import com.avokin.ideaLogViewer.lang.lexer.IdeaLogLexer;
+import com.intellij.execution.ui.ConsoleViewContentType;
 import com.intellij.lexer.Lexer;
 import com.intellij.openapi.editor.DefaultLanguageHighlighterColors;
-import com.intellij.openapi.editor.HighlighterColors;
 import com.intellij.openapi.editor.colors.TextAttributesKey;
 import com.intellij.openapi.fileTypes.SyntaxHighlighterBase;
 import com.intellij.psi.tree.IElementType;
@@ -19,11 +19,11 @@ public class IdeaLogSyntaxHighlighter extends SyntaxHighlighterBase {
 
     static {
         ATTRIBUTES.put(LOG_LEVEL_DEBUG, DefaultLanguageHighlighterColors.LINE_COMMENT);
-        ATTRIBUTES.put(LOG_LEVEL_INFO, DefaultLanguageHighlighterColors.METADATA);
-        ATTRIBUTES.put(LOG_LEVEL_WARN, DefaultLanguageHighlighterColors.BRACKETS);
-        ATTRIBUTES.put(LOG_LEVEL_ERROR, HighlighterColors.BAD_CHARACTER);
+        ATTRIBUTES.put(LOG_LEVEL_INFO, ConsoleViewContentType.SYSTEM_OUTPUT_KEY);
+        ATTRIBUTES.put(LOG_LEVEL_WARN, ConsoleViewContentType.LOG_WARNING_OUTPUT_KEY);
+        ATTRIBUTES.put(LOG_LEVEL_ERROR, ConsoleViewContentType.LOG_ERROR_OUTPUT_KEY);
         ATTRIBUTES.put(TIME_STAMP, DefaultLanguageHighlighterColors.NUMBER);
-        ATTRIBUTES.put(TEXT, DefaultLanguageHighlighterColors.STATIC_FIELD);
+        ATTRIBUTES.put(TEXT, ConsoleViewContentType.NORMAL_OUTPUT_KEY);
         ATTRIBUTES.put(MESSAGE, DefaultLanguageHighlighterColors.STRING);
         ATTRIBUTES.put(CLASS_NAME, DefaultLanguageHighlighterColors.CLASS_NAME);
         ATTRIBUTES.put(UPTIME, DefaultLanguageHighlighterColors.NUMBER);
