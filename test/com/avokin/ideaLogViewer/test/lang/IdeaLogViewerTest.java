fe7882143926fbd565b5824011730a26788ed7bc
Andrey Vokin <andrey.vokin@gmail.com>
2017-11-12 19:04:51 +0100

    Cleanup: removed unused imports

--- test/com/avokin/ideaLogViewer/test/lang/IdeaLogViewerTest.java
+++ test/com/avokin/ideaLogViewer/test/lang/IdeaLogViewerTest.java
@@ -3,11 +3,8 @@ package com.avokin.ideaLogViewer.test.lang;
 import com.avokin.ideaLogViewer.lang.lexer.IdeaLogLexer;
 import com.avokin.ideaLogViewer.test.IdeaLogViewerTestUtil;
 import com.intellij.lexer.Lexer;
-import com.intellij.openapi.application.PathManager;
 import com.intellij.testFramework.LexerTestCase;
 import org.jetbrains.annotations.NotNull;
-
-import java.io.File;
 
 public class IdeaLogViewerTest extends LexerTestCase {
   public void testGeneral() {
